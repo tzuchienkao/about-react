ab4fa795ef78055a8e45ab97505895a833e2d1aa
agnes <agnes@indochat.co.id>
2021-12-12 20:54:44 +0800

    about 切版

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,16 +1,37 @@
+import styled from 'styled-components';
 import about from '../datas/about';
 import LinkCard from '../components/LinkCard';
+const StyleSection = styled.section
+`
+  height: 100vh;
+  padding: 40px;
+  box-sizing: border-box;
+  display: grid;
+  gap: 40px;
+  grid-template: minmax(min-content, max-content) 1fr / repeat(2, 1fr);
+  img {
+    width: 100%;
+  }
+`
 
 const About = () => {
-  return Object.keys(about).map((data, idx) => {
-    return (
-      <LinkCard
-        key={idx}
-        title={about[data].title}
-        content={about[data].content}
-      />
-    )
-  })
+  return (
+    <StyleSection>
+      <img src="img/about.jpg" alt="" />
+      <div></div>
+      {
+        Object.keys(about).map((data, idx) => {
+          return (
+            <LinkCard
+              key={idx}
+              title={about[data].title}
+              content={about[data].content}
+            />
+          )
+        })
+      }
+    </StyleSection>
+  )
 }
 
 export default About;
