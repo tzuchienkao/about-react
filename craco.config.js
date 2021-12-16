804053bd4caccfebb461592c10ce252afd963b82
agnes <agnes@indochat.co.id>
2021-12-16 12:06:19 +0800

    fix public path, update website meta

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  webpack: {
+    // 返回webpack的配置信息
+    configure: (webpackConfig, { env, paths }) => {
+      webpackConfig.output.publicPath = '/about-react/'
+      return webpackConfig
+    }
+  },
   style: {
     postcss: {
       plugins: [
